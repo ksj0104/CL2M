525571ddb4f28b2e84a239c935609207fec52865
ksj0104 <154309849+ksj0104@users.noreply.github.com>
2024-07-12 10:08:28 +0900

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -5,7 +5,7 @@ import pytesseract
 import sys
 import openpyxl
 
-def write_data_to_excel(dict_data, excel_path = "./table.xlsx"):
+def write_data_to_excel(dict_data, excel_path):
 
     if os.path.exists(excel_path):
         workbook = openpyxl.load_workbook(excel_path)
@@ -13,24 +13,24 @@ def write_data_to_excel(dict_data, excel_path = "./table.xlsx"):
     else:
         workbook = openpyxl.Workbook()
         sheet = workbook.active
-        sheet.append(["이름","결제일자","금액","일일이용권","암벽화대여","장비구매"])
+        sheet.append(["이름","결제일자","금액","일일이용권","장비구매","이미지경로"])
 
     sheet.append([dict_data[0], dict_data[1], dict_data[2],
                   "O" if dict_data[3] == 'true' else ' ',
                   "O" if dict_data[4] == 'true' else ' ',
-                  "O" if dict_data[5] == 'true' else ' '
-                  ])
+                  dict_data[5]])
     workbook.save(excel_path)
+    workbook.close()
     return
 
-def ocr(image_path, name, day_, shoe, buy):
+def ocr(image_path, name, day_, buy):
     img = cv2.imread(image_path)
     custom_config = r'--oem 3 -l kor --psm 6'  # Tesseract OCR 설정 (선택적)
     text = pytesseract.image_to_string(img, config=custom_config).split('\n')
     date_tag = ["거래일시","결제일","거래일자","결제일시","이용일시"]
     price_tag = ["승인금액","결제금액","매입금액","이용금액","받은금액"]
 
-    dict = [name, "", "", day_, shoe, buy]
+    dict = [name, "", "", day_, buy, image_path]
 
     for line in text:
         _str = line.rstrip().replace(" ","")
@@ -61,7 +61,8 @@ def ocr(image_path, name, day_, shoe, buy):
     # print(dict)
     if dict[2] == "" or dict[1] == "":
         return
-    write_data_to_excel(dict, "./table.xlsx")
+    write_data_to_excel(dict, f"./data_{YY}{MM}.xlsx")
+
     return
 
 
@@ -70,7 +71,6 @@ if __name__ == "__main__":
     img_path = sys.argv[1]
     name = sys.argv[2]
     day_ = sys.argv[3]
-    shoe = sys.argv[4]
-    buy = sys.argv[5]
+    buy = sys.argv[4]
 
-    ocr(img_path, name, day_, shoe, buy)
+    ocr(img_path, name, day_, buy)
