b33d983a2bf2754a1e0dec67d3987fc8b25f359d
ksj0104 <154309849+ksj0104@users.noreply.github.com>
2024-07-12 10:08:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
@@ -7,6 +8,7 @@ const { spawn } = require('child_process');
 const ExcelJS = require('exceljs');
 const Hangul = require('hangul-js');
 const archiver = require('archiver');
+const moment = require('moment');
 
 // 사용자 세션을 저장할 객체
 const sessions = {};
@@ -17,13 +19,22 @@ const https_host = "10.10.30.241"
 const ws_host = "10.10.30.241"
 const wss = new WebSocket.Server({ host: ws_host, port: 3030 });
 const UPLOAD_DIR = "imgs"
-const EXCEL_FILE = path.join(__dirname, "table.xlsx");
+
+function get_excel_path(){
+    const now = moment();
+    const year = now.format('YYYY');
+    const month = now.format('MM');
+    return `data_${year}${month}.xlsx`;
+}
 
 // HTTP 서버 생성
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     // 쿠키를 파싱하여 세션 아이디를 추출합니다.
     const cookies = parseCookies(req);
     let sessionID = cookies.sessionID;
+
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
 
     // 세션 아이디가 없으면 새로 생성합니다.
     if (!sessionID) {
@@ -39,7 +50,7 @@ const server = http.createServer((req, res) => {
         };
     }
 
-    if (req.url === '/') {
+    if (pathname === '/') {
         let filePath = path.join(__dirname, 'index_m.html');
         // 파일을 비동기적으로 읽습니다.
         fs.readFile(filePath, (err, data) => {
@@ -55,8 +66,13 @@ const server = http.createServer((req, res) => {
             }
         });
     }
-    else if (req.url === '/admin/excel_download') {
-        fs.access(EXCEL_FILE, fs.constants.F_OK, (err) => {
+    else if (pathname === '/admin/excel_download') {
+
+        const year = parsedUrl.query.year;
+        const month = parsedUrl.query.month;
+        const xlsx_path = path.join(__dirname, `data_${year}${month}.xlsx`)
+        console.log(xlsx_path + "다운로드 요청")
+        fs.access(xlsx_path, fs.constants.F_OK, (err) => {
             if (err) {
                 res.writeHead(404, { 'Content-Type': 'text/plain' });
                 res.end('File not found');
@@ -64,38 +80,37 @@ const server = http.createServer((req, res) => {
             }
             res.writeHead(200, {
                 'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                'Content-Disposition': 'attachment; filename=' + EXCEL_FILE
+                'Content-Disposition': 'attachment; filename=' + `data_${year}${month}.xlsx`
             });
-            fs.createReadStream(EXCEL_FILE).pipe(res);
+            fs.createReadStream(xlsx_path).pipe(res);
         });
     }
-    else if (req.url === '/admin/imgs_download') {
-        const imagesDir = path.join(__dirname, 'imgs');
-        const zipFilePath = path.join(__dirname, 'images.zip');
-        const output = fs.createWriteStream(zipFilePath);
-        const archive = archiver('zip', {
-            zlib: { level: 9 } // compression level
-        });
+    else if (pathname === '/admin/imgs_download') {
 
-        output.on('close', () => {
+
+        const year = parsedUrl.query.year;
+        const month = parsedUrl.query.month;
+        const xlsx_path = path.join(__dirname, `data_${year}${month}.xlsx`)
+        try {
+            const imagePaths = await readLastColumn(xlsx_path);
+            const zipFilePath = path.join(__dirname, 'images.zip');
+            await createZipFromImages(imagePaths, zipFilePath);
+
             res.writeHead(200, {
                 'Content-Type': 'application/zip',
-                'Content-Disposition': 'attachment; filename=images.zip',
-                'Content-Length': archive.pointer()
+                'Content-Disposition': `attachment; filename=${path.basename(zipFilePath)}`
             });
-            fs.createReadStream(zipFilePath).pipe(res);
-        });
 
-        archive.on('error', (err) => {
+            const readStream = fs.createReadStream(zipFilePath);
+            readStream.pipe(res);
+        } catch (error) {
+            console.error('오류가 발생했습니다:', error);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error creating zip file');
-        });
+            res.end('Internal Server Error');
+        }
 
-        archive.pipe(output);
-        archive.directory(imagesDir, false);
-        archive.finalize();
     }
-    else if (req.url === '/admin') {
+    else if (pathname === '/admin') {
         let filePath = path.join(__dirname, 'cl2m_admin.html');
         // 파일을 비동기적으로 읽습니다.
         fs.readFile(filePath, (err, data) => {
@@ -110,7 +125,7 @@ const server = http.createServer((req, res) => {
             }
         });
     }
-    else if (req.url === '/admin.js') {
+    else if (pathname === '/admin.js') {
       fs.readFile('admin.js', (err, data) => {
         if (err) {
           res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -121,7 +136,7 @@ const server = http.createServer((req, res) => {
         res.end(data);
       });
     }
-    else if (req.url === '/script.js') {
+    else if (pathname === '/script.js') {
       fs.readFile('script.js', (err, data) => {
         if (err) {
           res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -144,23 +159,21 @@ const port = 3000;
 server.listen(port, https_host, () => {
     console.log(`Server running at http://${https_host}:${port}/`);
 });
-create_file(EXCEL_FILE)
 
 function create_file(filepath) {
-
     // 파일 존재 여부 확인
     if (!fs.existsSync(filepath)) {
         const workbook = new ExcelJS.Workbook();
         const worksheet = workbook.addWorksheet('Sheet1');
 
-        // 테이블 헤더 추가
+        // 테이블 헤더 추가 "이름","결제일자","금액","일일이용권","장비구매","이미지경로"
         worksheet.columns = [
             {header: '이름', key: '이름', width: 30},
             {header: '결제일자', key: '결제일자', width: 30},
             {header: '금액', key: '금액', width: 30},
             {header: '일일이용권', key: '일일이용권', width: 30},
-            {header: '암벽화대여', key: '암벽화대여', width: 30},
-            {header: '장비구매', key: '장비구매', width: 30}
+            {header: '장비구매', key: '장비구매', width: 30},
+            {header: '이미지경로', key: '이미지경로', width: 30}
         ];
 
         // 엑셀 파일 쓰기
@@ -171,8 +184,6 @@ function create_file(filepath) {
             .catch((err) => {
                 console.error('파일 생성 중 오류가 발생했습니다:', err);
             });
-    } else {
-        console.log('엑셀 파일이 이미 존재합니다.');
     }
 }
 // 쿠키를 파싱하는 함수
@@ -227,26 +238,28 @@ wss.on('connection', function connection(ws) {
             break;
         }
     }
-    readExcelFile(EXCEL_FILE, ws);
+    // default 는 현재 년월로 구성된 파일을 읽어온다.
+    readExcelFile(get_excel_path(), ws);
     ws.on('message', function incoming(message) {
         const data = JSON.parse(message);
         if (data.type === 'image') {
-
             const buffer = Buffer.from(data.imageData, 'base64');
             const username = data.username;
-            const expe = data.filename.split('.')[1]
+            const expe = data.filename.split('.')[1];
             const file_name = convertHangulToEnglish(username) +'_'+generateSecureRandomString(8) + '.' + expe;
             const file_path = path.join(UPLOAD_DIR, file_name);
-
             fs.writeFile(file_path, buffer, err => {
                 if (err) {
                     ws.send(JSON.stringify({ type: 'error', message: 'File save error' }));
                 }
                 else {
                     ws.send(JSON.stringify({ type: 'success', message: 'File uploaded successfully' }));
-                    ocr(file_path, username, data.day_use, data.shoe_rent, data.buy_equi);
+                    ocr(file_path, username, data.day_use, data.buy_equi, ws);
                 }
           });
+        }
+        else if(data.type === 'YYYYMM'){ // 캘린더에 해당하는 파일 불러오기
+            readExcelFile(`data_${data.year}${data.month}.xlsx`, ws);
         }
     });
 
@@ -259,9 +272,9 @@ wss.on('connection', function connection(ws) {
 });
 
 
-function ocr(image_path, user_name, day_, shoe, buy){
-    console.log(image_path, user_name);
-    const pythonProcess = spawn('python', ['ocr.py', image_path, user_name, day_, shoe, buy]);
+function ocr(image_path, user_name, day_, buy){
+    // console.log(image_path, user_name);
+    const pythonProcess = spawn('python', ['ocr.py', image_path, user_name, day_, buy]);
     pythonProcess.stdout.on('data', (data) => {
         console.log(data)
     });
@@ -272,35 +285,11 @@ function ocr(image_path, user_name, day_, shoe, buy){
 
     pythonProcess.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
-        readLastData(EXCEL_FILE)
     });
 }
-
-async function readLastData(filename) {
-    const workbook = new ExcelJS.Workbook();
-    try {
-    // 엑셀 파일 로드
-        await workbook.xlsx.readFile(filename);
-
-    // 첫 번째 시트 선택 (index는 1부터 시작)
-        const worksheet = workbook.getWorksheet(1);
-        // 첫 번째 행과 마지막 행 가져오기
-        const headers = worksheet.getRow(1).values.slice(1); // 첫 번째 행의 값들 (첫 번째 열은 빈 값이므로 제외)
-        const lastRowValues = worksheet.getRow(worksheet.rowCount).values.slice(1); // 마지막 행의 값들 (첫 번째 열은 빈 값이므로 제외)
-
-        // 첫 번째 행을 키로, 마지막 행을 값으로 변환
-        const result = {};
-            headers.forEach((header, index) => {
-            result[header] = lastRowValues[index];
-        });
-        broadcastMSG("update_table", JSON.stringify(result));
-
-    } catch (error) {
-        console.error('엑셀 파일 읽기 실패:', error);
-    }
-}
-
 async function readExcelFile(filename, ws) {
+    // 없으면 파일 생성
+    create_file(filename);
     const workbook = new ExcelJS.Workbook();
     try {
     // 엑셀 파일 로드
@@ -334,6 +323,69 @@ async function readExcelFile(filename, ws) {
     }
 }
 
+// 엑셀 파일에서 이미지 경로 읽기 함수
+async function readLastColumn(filePath) {
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(filePath);
+
+    const worksheet = workbook.getWorksheet(1); // 첫 번째 워크시트 선택
+
+    const imagePaths = [];
+
+    worksheet.eachRow((row, rowNumber) => {
+        if (rowNumber > 1) { // 두 번째 행부터 읽기
+            const lastCell = row.getCell(row.cellCount);
+            if (lastCell.value) {
+                imagePaths.push(lastCell.value);
+            }
+        }
+    });
+
+    return imagePaths;
+}
+
+// 이미지 경로 리스트를 사용하여 압축 파일 생성 함수
+function createZipFromImages(imagePaths, outputZipPath) {
+    return new Promise((resolve, reject) => {
+        if (!Array.isArray(imagePaths)) {
+            return reject(new TypeError('imagePaths should be an array'));
+        }
+
+        // 출력 스트림 생성
+        const output = fs.createWriteStream(outputZipPath);
+        const archive = archiver('zip', {
+            zlib: { level: 9 } // 최고 압축 수준
+        });
+
+        // 스트림 이벤트 핸들러 설정
+        output.on('close', () => {
+            console.log(`압축 파일이 생성되었습니다: ${outputZipPath} (${archive.pointer()} total bytes)`);
+            resolve();
+        });
+
+        archive.on('warning', (err) => {
+            if (err.code !== 'ENOENT') {
+                console.warn(err);
+            }
+        });
+
+        archive.on('error', (err) => {
+            reject(err);
+        });
+
+        // 아카이브와 출력 스트림 연결
+        archive.pipe(output);
+
+        // 이미지 파일들을 압축에 추가
+        imagePaths.forEach((imagePath) => {
+            const fileName = path.basename(imagePath);
+            archive.file(imagePath, { name: fileName });
+        });
+
+        // 압축 완료
+        archive.finalize();
+    });
+}
 
 function generateSecureRandomString(length) {
   return crypto.randomBytes(length).toString('hex').slice(0, length);
