ec8ebc3c18e4095301ea58bbf16b40980dc77f5d
ksj0104 <154309849+ksj0104@users.noreply.github.com>
2024-08-07 15:16:14 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://3.34.113.208:3030');
+const ws = new WebSocket('ws://10.10.30.241:3030');
 ws.onopen = function(event) {
     console.log('Connected to WebSocket server');
 };
